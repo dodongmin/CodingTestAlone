ba3eb2ac543ff26b292c01b75d57ad1db6a2b0cd
dodongmin <ghkandud@gmail.com>
2024-03-05 23:11:33 +0900

    2주차 백준

---
+++ BaekJoon/13335.py
@@ -0,0 +1,5 @@
+n, w, l = map(int, input().split())
+truck = list(input().split())
+
+print(n, w, l)
+print(truck)
---
+++ BaekJoon/3085.py
@@ -0,0 +1,4 @@
+a = int(input())
+candy_list = [list(input()) for _ in range(a)]
+
+print(candy_list)

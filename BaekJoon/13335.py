25a7f189e9e5d39b83e1f96cbfe85a909ac2e857
dodongmin <ghkandud@gmail.com>
2024-03-07 00:41:21 +0900

    133365 1차 생각

--- BaekJoon/13335.py
+++ BaekJoon/13335.py
@@ -1,5 +1,21 @@
 n, w, l = map(int, input().split())
-truck = list(input().split())
+truck = list(map(int, input().split()))
+time = 0
 
-print(n, w, l)
-print(truck)
+for i in range(1,n):
+    if sum(truck) == l:
+        time = n + w
+    elif len(truck) == 1:
+        time = n + w
+    else:
+        if sum(truck[:i-1]) <= l and sum(truck[:i]) > l:
+            time += w + len(truck[:i-1])
+            
+            del truck[:i-1]
+            print(time)
+            print(truck)
+            print(len(truck))
+        if len(truck) == 1:
+            time += 1
+
+print(time)

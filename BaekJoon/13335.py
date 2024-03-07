7abcfec723c2d8cc85e4051f3824757923991223
dodongmin <ghkandud@gmail.com>
2024-03-07 13:39:11 +0900

    13335 두번째 알고리즘

--- BaekJoon/13335.py
+++ BaekJoon/13335.py
@@ -2,21 +2,23 @@ n, w, l = map(int, input().split())
 truck = list(map(int, input().split()))
 time = 0
 
-for i in range(1,n):
-    if sum(truck) == l:
-        time = n + w
-    elif len(truck) == 1:
-        time = n + w
-    else:
-        if sum(truck[:i-1]) <= l and sum(truck[:i]) > l:
-            time += w + len(truck[:i-1])
-            
-            del truck[:i-1]
-            print(time)
-            print(truck)
-            print(len(truck))
-        if len(truck) == 1:
-            time += 1
+if sum(truck) == l:
+    time += n + w
+elif len(truck) == 1:
+    time += n + w
+else:
+     
+    for i in range(1,n):
+        if sum(truck) == l:
+            time += n + w
+        elif len(truck) == 1:
+            time += n + w
+        else:
+            if sum(truck[:i-1]) <= l and sum(truck[:i]) > l:
+                time += w + len(truck[:i-1])
+                
+                del truck[:i-1]
+            if len(truck) == 1:
+                time += 1
 
 print(time)
-#

82567111edd4185555579fa9509233898b9815ba
dodongmin <ghkandud@gmail.com>
2024-03-14 00:12:14 +0900

    11724

--- BaekJoon/11724.py
+++ BaekJoon/11724.py
@@ -1,19 +1,17 @@
 import sys
-
 sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
 n, m = [*map(int,input().split())]
 graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+count = 0
 
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
     
-
-visited = [0] * (n+1)
-
 def dfs(v):
     visited[v] = 1
     for i in graph[v]:
@@ -21,7 +19,6 @@ def dfs(v):
             visited[i] = 1
             dfs(i)
 
-count = 0
 for i in range(1, n+1):
     if visited[i] == 0:
         count += 1

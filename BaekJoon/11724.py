50e9bb1f44ab7ebd9c9553b72bd9db65eb282799
dodongmin <ghkandud@gmail.com>
2024-03-13 23:59:00 +0900

    11724 완

--- BaekJoon/11724.py
+++ BaekJoon/11724.py
@@ -1,24 +1,24 @@
 n, m = [*map(int,input().split())]
-
 graph = [[] for _ in range(n+1)]
+
 for _ in range(m):
-    s, e = map(int, input().split())
-    graph[s].append(e)
-    graph[e].append(s)
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
     
 
-visited = [False] * (n+1)
+visited = [0] * (n+1)
 
 def dfs(v):
-    visited[v] = True
+    visited[v] = 1
     for i in graph[v]:
-        if visited[i] == False:
-            visited[i] = True
+        if visited[i] == 0:
+            visited[i] = 1
             dfs(i)
 
 count = 0
 for i in range(1, n+1):
-    if visited[i] == False:
+    if visited[i] == 0:
         count += 1
         dfs(i)
         
